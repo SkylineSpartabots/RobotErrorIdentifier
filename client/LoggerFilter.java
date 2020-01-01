2962c15bc76118b8b6a47b0509831e7458aeee27
Flashynurav <varunananth1@gmail.com>
2020-01-01 12:50:34 -0800

    Version 1.0! Basic features all implemented.
    The client can now check for the most recent file in the .dsevents directory, or choose its own file to read if provided with a filename. The output file also now reflects the file read, not the file made as output to make it easier to find which output relates to which .dsevents file. All code has been thoroughly documented.

--- client/LoggerFilter.java
+++ client/LoggerFilter.java
@@ -1,22 +1,69 @@
 package client;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
 
+/**
+ * A client that can be run at the end of matches to parse through .dsevents files and output a ".txt" file that only contains important information about robot malfunctions.
+ * Helpful for post-match diagnostics.
+ * @version
+ * 1.0s
+ * @author
+ * Team 2976!
+ */
 public class LoggerFilter {
-    //TODO: N/A for now
-    private static final String folderPath = "C:\\Users\\Public\\Documents\\FRC\\Log Files\\"; //Location of all .dsevents files
-    private static final String fileName = ""; //Paste the name of the file you want to parse here
+    /**
+     * Location of all .dsevents files, filepath can be found through Driver Station Log File Viewer. 
+     */
+    private static final String folderPath = "C:\\Users\\Public\\Documents\\FRC\\Log Files\\";
+    /**
+     * Set to "" if you want to get the most recent file, set to a filename if you want to read that specific file. 
+     * Be sure to add ".dsevents" to the end of the filename.
+     */
+    private static String fileName = "";
+    /**
+     * Upper bound to use when parsing for errors.
+     */
+    private static final String ALERT_KEY_UPPER_BOUND = "!!!!!!!! Error:";
+    /**
+     * Lower bound to use when parsing for errors.
+     */
+    private static final String ALERT_KEY_LOWER_BOUND = "!!!!!!!!";
 
     public static void main(final String[] args) {
+        if(fileName.equals("")) {
+            getMostRecentFile();
+        }
+        readFile();
+    }
+    /**
+     * Uses the .lastModified(); method to get the name of the most recently created .dsevents file.
+     * Only does this if the class variable "fileName" is blank ("").
+     */
+    public static void getMostRecentFile() {
+        final File directory = new File(folderPath);
+        final File[] allFiles = directory.listFiles();
+        long lastModTime = allFiles[0].lastModified();
+        File mostRecentFile = allFiles[0];
+        for (final File f : allFiles) {
+            if(f.lastModified() > lastModTime) {
+                lastModTime = f.lastModified();
+                mostRecentFile = f;
+            }
+            fileName = mostRecentFile.getName();
+        }
+    }
+    /**
+     * Reads the file in as a stream of bytes and concatenates the String information from these bytes onto a String called "allText".
+     */
+    public static void readFile() {
         try {
             final byte[] buffer = new byte[1024];
-            final FileInputStream fis = new FileInputStream(folderPath + fileName + ".dsevents");
+            final FileInputStream fis = new FileInputStream(folderPath + fileName);
             String allText = "";
             while (fis.read(buffer) != -1) {
                 String nextLine = null;
@@ -26,20 +73,24 @@ public class LoggerFilter {
             parseData(allText);
             fis.close();
         } catch (final FileNotFoundException e) {
-            System.out.println("Failed to find file");
+            System.out.println("Failed to find file.");
             e.printStackTrace();
         } catch (final IOException e) {
-            System.out.println("Failed to read file");
+            System.out.println("Failed to read file.");
             e.printStackTrace();
         }
     }
-
+    /**
+     * Parses through the .dsevents file as a String and looks for specific error messages bounded by "ALERT_KEY_UPPER_BOUND" and "ALERT_KEY_LOWER_BOUND"
+     * (These variables can be found at the top of the class as class constants). 
+     * It then adds these parsed and filtered error messages to an ArrayList<String> object.
+     * @param s -> The .dsevents file as a String.
+     * @throws IOException
+     */
     public static void parseData(String s) throws IOException {
-        final String alertKeyUpperBound = "!!!!!!!! Error:";
-        final String alertKeyLowerBound = "!!!!!!!!";
         final ArrayList<String> allMessages = new ArrayList<String>();
-        s = s.replaceAll(alertKeyUpperBound, "<S>");
-        s = s.replaceAll(alertKeyLowerBound, "<E>");
+        s = s.replaceAll(ALERT_KEY_UPPER_BOUND, "<S>");
+        s = s.replaceAll(ALERT_KEY_LOWER_BOUND, "<E>");
         while (s.contains("<S>") && s.contains("<E>")) {
             final int a = s.indexOf("<S>");
             final int b = s.indexOf("<E>") + 3;
@@ -49,14 +100,14 @@ public class LoggerFilter {
         }
         writeToFile(allMessages);
     }
-
+    /**
+     * Writes an ArrayList<String> of parsed errors to an output file.
+     * For more information on the output file, check "README.txt" in the output folder.
+     * @param list -> The ArrayList<String> object that will be printed to the file.
+     * @throws IOException
+     */
     public static void writeToFile(final ArrayList<String> list) throws IOException {
-        final Date currentDate = new Date();
-        final Calendar currentCal = Calendar.getInstance();
-        currentCal.setTime(currentDate);
-        final String fileName = "LOG_DATE_" + currentCal.get(Calendar.YEAR) + "_" + currentCal.get(Calendar.MONTH) + "_"
-                + currentCal.get(Calendar.DAY_OF_MONTH) + "_TIME_" + currentCal.get(Calendar.HOUR_OF_DAY) + "_"
-                + currentCal.get(Calendar.MINUTE) + "_" + currentCal.get(Calendar.SECOND);
+        final String fileName = LoggerFilter.fileName + " ROBOT_ERROR_IDENTIFIER";
 
         final String filePath = "output\\" + fileName;
         final FileWriter fw = new FileWriter(filePath, false);
